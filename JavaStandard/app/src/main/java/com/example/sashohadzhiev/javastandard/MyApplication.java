77b679d847bf70ec27dbb3463ddbeb774ccdcd0e
shadzhiev <sasho.hadzhiev@yahoo.com>
2017-06-29 18:59:20 +0300

    android manifest parser test 2. - modifications

--- JavaStandard/app/src/main/java/com/example/sashohadzhiev/javastandard/MyApplication.java
+++ JavaStandard/app/src/main/java/com/example/sashohadzhiev/javastandard/MyApplication.java
@@ -31,12 +31,6 @@ public class MyApplication extends Application {
     public void onCreate() {
         super.onCreate();
 
-            StrictMode.setThreadPolicy(new StrictMode.ThreadPolicy.Builder()
-                    .detectDiskReads()
-                    .detectDiskWrites()
-                    .detectNetwork()   // or .detectAll() for all detectable problems
-                    .penaltyLog()
-                    .build());
             StrictMode.setVmPolicy(new StrictMode.VmPolicy.Builder()
                     .detectLeakedSqlLiteObjects()
                     .detectLeakedClosableObjects()
